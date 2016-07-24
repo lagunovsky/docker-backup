573860efd24684a3bba4c42ad123f36d4235744b
Ivan Lagunovsky <ivan.ofical@gmail.com>
2016-07-24 17:49:15 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,28 +2,29 @@
 
 set -e
 
-: ${AWS_ACCESS_KEY_ID:?"AWS_ACCESS_KEY_ID env variable is required"}
-: ${AWS_SECRET_ACCESS_KEY:?"AWS_SECRET_ACCESS_KEY env variable is required"}
-
 CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
 
-{
-  echo '[default]';
-  echo "access_key=$AWS_ACCESS_KEY_ID";
-  echo "secret_key=$AWS_SECRET_ACCESS_KEY";
-  echo "default_mime_type = binary/octet-stream"
-} > ~/.s3cfg
-
-if [ -n "$GPG_PASSPHRASE" ]
+if [ -n "$AWS_ACCESS_KEY_ID" ]
 then
-  echo "gpg_command = /usr/bin/gpg" >> ~/.s3cfg
-  echo "gpg_passphrase = $GPG_PASSPHRASE" >> ~/.s3cfg
+  {
+    echo '[default]';
+    echo "access_key=$AWS_ACCESS_KEY_ID";
+    echo "secret_key=$AWS_SECRET_ACCESS_KEY";
+    echo "default_mime_type = binary/octet-stream"
+  } > ~/.s3cfg
+  
+  if [ -n "$GPG_PASSPHRASE" ]
+  then
+    echo "gpg_command = /usr/bin/gpg" >> ~/.s3cfg
+    echo "gpg_passphrase = $GPG_PASSPHRASE" >> ~/.s3cfg
+  fi
+  
+  if [ -n "$AWS_LOCATION" ]
+  then
+    echo "bucket_location=$AWS_LOCATION" >> ~/.s3cfg
+  fi
 fi
 
-if [ -n "$AWS_LOCATION" ]
-then
-  echo "bucket_location=$AWS_LOCATION" >> ~/.s3cfg
-fi
 
 if [[ "$1" == 'no-cron' ]]; then
     exec /duplicity-backup.sh --backup
