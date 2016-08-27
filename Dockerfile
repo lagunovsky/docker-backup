6f6398d1b1e5df4b1c4b13a8ffc59b08364f800e
lagun4ik <ivan@lagunovsky.com>
2016-08-27 16:26:04 +0300

    fix cron

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER ivan@lagunovsky.com
 RUN apt-get update \
     && apt-get upgrade -y \
     && apt-get install -y --no-install-recommends duplicity ca-certificates s3cmd haveged apg python-boto python-paramiko cron \
-    && apt-get autoclean 
+    && apt-get autoclean
 
 ADD /duplicity-backup/duplicity-backup.sh /duplicity-backup.sh
 RUN chmod +x /duplicity-backup.sh
--- start.sh
+++ start.sh
@@ -12,13 +12,13 @@ then
     echo "secret_key=$AWS_SECRET_ACCESS_KEY";
     echo "default_mime_type = binary/octet-stream"
   } > ~/.s3cfg
-  
+
   if [ -n "$GPG_PASSPHRASE" ]
   then
     echo "gpg_command = /usr/bin/gpg" >> ~/.s3cfg
     echo "gpg_passphrase = $GPG_PASSPHRASE" >> ~/.s3cfg
   fi
-  
+
   if [ -n "$AWS_LOCATION" ]
   then
     echo "bucket_location=$AWS_LOCATION" >> ~/.s3cfg
@@ -33,7 +33,7 @@ else
     if [[ ! -e "$LOGFIFO" ]]; then
         mkfifo "$LOGFIFO"
     fi
-    echo -e "$CRON_SCHEDULE /duplicity-backup.sh --backup > $LOGFIFO 2>&1" | crontab -
+    echo -e "$CRON_SCHEDULE /duplicity-backup.sh --backup --config /duplicity-backup.conf > $LOGFIFO 2>&1" | crontab -
     crontab -l
     cron
     tail -f "$LOGFIFO"
